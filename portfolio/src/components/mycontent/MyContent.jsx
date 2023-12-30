6ae9e4474e995230b902ff8e8a1e8b11effef7a8
ayush ranjan <ayushranjan656@gmail.com>
2023-12-31 02:20:06 +0530

    'r'

--- portfolio/src/components/mycontent/MyContent.jsx
+++ portfolio/src/components/mycontent/MyContent.jsx
@@ -7,7 +7,7 @@ const MyContent = () => {
             <h2 className='h2my'> Ayush Ranjan.</h2>
             <h3 >I love exploring new things!</h3>
             <div className='leftmy' ><p>I'm a Final Year Computer Science Engineering Student at NIT, Jalandhar. Primarily interested in Web Development and Data Science.</p>
-                <p>I enjoy learning new skills and implementing them in real life!</p></div>
+                <p>I enjoy learning new skills and implementing them in real life !!</p></div>
                 <div className='touch'>Get in Touch</div>
         </div>
     )
